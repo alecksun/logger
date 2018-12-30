05497c6f0336d662157e8a669d23abbe824c30b0
Aleck Sun <alecksun@gmail.com>
2018-12-30 21:30:51 +0800

    Add color to output

--- test/test.js
+++ test/test.js
@@ -11,6 +11,18 @@ logger.info('message1', 'message2');
 logger.module = 'test2';
 logger.error('message1', 'message2');
 
+logger.fatal('message1', 'message2');
+console.log("should be normal");
+
+logger = logger.createLogger({
+    property1: {
+        key1: 'value-1',
+        key2: ['value-2', 'value-3']
+    }
+});
+logger.info('Should be string');
+logger.jsonFormat(true);
+logger.info('Should be json');
 
 logger = new Logger('test', Logger.LogLevels.WARN);
 logger.error('should show');
